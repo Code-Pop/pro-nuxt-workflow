e74d671d0a6555385d6b7af43f926941960cd28a
davidchuka <bigbochuks@gmail.com>
2024-08-29 16:11:49 +0100

    update L6 end code

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,19 @@
+<script setup>
+const { path } = useRoute()
+const { data: blogPost } = await useAsyncData(`content-${path}`, () => {
+  return queryContent().where({ _path: path }).findOne()
+})
+</script>
 <template>
+  <article>
+    <header>
+      <h1>{{ blogPost.title }}</h1>
+      <p>{{ blogPost.description }}</p>
+      <p>By {{ blogPost.author }}</p>
+    </header>
     <main>
       <ContentDoc />
     </main>
+  </article>
+
 </template>
