8d69c33e4aed7f453c99ad42ba2a5fe34987a03b
davidchuka <bigbochuks@gmail.com>
2024-08-29 16:20:41 +0100

    update L8 starter code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,5 @@
 export default defineNuxtConfig({
   devtools: {
     enabled: true,
-    customTabs: [
-      {
-        name: "histoire",
-        title: "Histoire",
-        icon: "/histoire-logo.svg",
-        view: {
-          type: "iframe",
-          src: "http://localhost:6006/",
-        },
-      },
-    ],
   },
 });
